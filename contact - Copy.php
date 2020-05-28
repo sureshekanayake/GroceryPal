ebb77de304446f774c1072459105002960b69588
Suresh Ekanayake <sureshekanayake@Sureshs-MacBook-Air.local>
2020-05-29 00:14:46 +0530

    Final Commit

---
+++ contact - Copy.php
@@ -0,0 +1,73 @@
+ <div id="contact-page" class="container">
+        <div class="bg">
+            <div class="row">           
+                <div class="col-sm-12">                         
+                    <h2 class="title text-center">Contact <strong>Us</strong></h2>                                                      
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.084655723661!2d79.86859331491648!3d6.880461420819073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259673a998193%3A0xe687bdb6f2a4d767!2sLaugfs%20Holdings!5e0!3m2!1sen!2slk!4v1590665499943!5m2!1sen!2slk" width="1140" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>                  
+            </div>      
+            <div class="row">   
+                <div class="col-sm-8">
+                    <div class="contact-form">
+                        <h2 class="title text-center">Get In Touch</h2>
+                        <div class="status alert alert-success" style="display: none"></div>
+                        <form action="feedback.php" method="post" class="agile_form">
+          <h2>How satisfied were you with our Service?</h2>
+             <ul class="agile_info_select">
+                 <li><input type="radio" name="view" value="excellent" id="excellent" required> 
+                      <label for="excellent">excellent</label>
+                      <div class="check w3"></div>
+                 </li>
+                 <li><input type="radio" name="view" value="good" id="good"> 
+                      <label for="good"> good</label>
+                      <div class="check w3ls"></div>
+                 </li>
+                 <li><input type="radio" name="view" value="neutral" id="neutral">
+                     <label for="neutral">neutral</label>
+                     <div class="check wthree"></div>
+                 </li>
+                 <li><input type="radio" name="view" value="poor" id="poor"> 
+                      <label for="poor">poor</label>
+                      <div class="check w3_agileits"></div>
+                 </li>
+             </ul>    
+            <h2>If you have specific feedback, please write to us...</h2>
+            <textarea placeholder="Additional comments" class="w3l_summary" name="comments" required=""></textarea>
+            <input type="text" placeholder="Your Name (optional)" name="name"  />
+            <input type="email" placeholder="Your Email (optional)" name="email"/>
+            <input type="text" placeholder="Your Number (optional)" name="num"  /><br>
+            <center><input type="submit" value="submit Feedback" class="agileinfo" /></center>
+      </form>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="contact-info">
+                        <h2 class="title text-center">Contact Info</h2>
+                        <address>
+                            <p>GroceryPal</p><br>
+                            <p>Lfinity Pvt Ltd, No 125, Maya avenue, Colombo 06, Sri Lanka</p><br>
+                            <p>Mobile: 076 8 336 333</p><br>
+   
+                            <p>Email: info@grocerypal.lk</p>
+                        </address>
+                        <div class="social-networks">
+                            <h2 class="title text-center">Social Networking</h2>
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/grocerypal.lk/"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>              
+            </div>  
+        </div>  
+    </div><!--/#contact-page-->
---
+++ home.php
@@ -0,0 +1,262 @@
+<section id="slider"><!--slider-->
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#slider-carousel" data-slide-to="1"></li>
+              <li data-target="#slider-carousel" data-slide-to="2"></li>
+            </ol>
+            
+            <div class="carousel-inner">
+              <div class="item active">
+                <div class="col-sm-6">
+                  <h1><span>GroceryPal</h1>
+                  <h2>Welcome to GroceryPal.lk</h2>
+                  <p>Select & Order Your Favourite Items Quickly. </p>
+                 
+                </div>
+                <div class="col-sm-6">
+                  <img src="images/home/cover1.jpg" class="girl img-responsive" alt="" />
+                </div>
+              </div>
+              <div class="item">
+                <div class="col-sm-6">
+                <h1><span>GroceryPal</h1>
+                  <h2>Choose Your Favourite Brands.</h2>
+                  
+                 
+                </div>
+                <div class="col-sm-6">
+                  <img src="images/home/cover2.png" class="girl img-responsive" alt="" />
+                </div>
+              </div>
+              
+              <div class="item">
+                <div class="col-sm-6">
+                <h1><span>GroceryPal</h1>
+                  <h2>24 - 48 HR Doorstep Delivery.</h2>
+                 
+                </div>
+                <div class="col-sm-6">
+                  <img src="images/home/cover3.jpg" class="girl img-responsive" alt="" />
+                </div>
+              </div>
+              
+            </div>
+            
+            <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+              <i class="fa fa-angle-left"></i>
+            </a>
+            <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+              <i class="fa fa-angle-right"></i>
+            </a>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </section><!--/slider-->
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3">
+            <?php include 'sidebar.php'; ?>
+        </div>
+        
+        <div class="col-sm-9 padding-right">
+          <div class="features_items"><!--features_items-->
+            <h2 class="title text-center">Top Picks</h2>
+
+            <?php
+
+            $query = "SELECT * FROM `tblpromopro` pr , `tblproduct` p , `tblcategory` c
+            WHERE pr.`PROID`=p.`PROID` AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 ";
+            $mydb->setQuery($query);
+            $cur = $mydb->loadResultList();
+           
+            foreach ($cur as $result) { 
+
+              ?>
+               <form   method="POST" action="cart/controller.php?action=add">
+            <input type="hidden" name="PROPRICE" value="<?php  echo $result->PROPRICE; ?>">
+            <input type="hidden" id="PROQTY" name="PROQTY" value="<?php  echo $result->PROQTY; ?>">
+
+            <input type="hidden" name="PROID" value="<?php  echo $result->PROID; ?>">
+            <div class="col-sm-4">
+              <div class="product-image-wrapper">
+                <div class="single-products">
+                    <div class="productinfo text-center">
+                      <img src="<?php  echo web_root.'admin/products/'. $result->IMAGES; ?>" alt="" />
+                      <h2>Rs. <?php  echo $result->PRODISPRICE; ?></h2>
+                      <p><?php  echo    $result->PRODESC; ?></p>
+                      <button type="submit" name="btnorder" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                    </div>
+                    <div class="product-overlay">
+                      <div class="overlay-content">
+                        <h2>Rs. <?php  echo $result->PRODISPRICE; ?></h2>
+                        <p><?php  echo    $result->PRODESC; ?></p>
+                       <button type="submit" name="btnorder" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                      </div>
+                    </div>
+                </div>
+                <div class="choose">
+                  <ul class="nav nav-pills nav-justified">
+                    <li>
+                              <?php     
+                            if (isset($_SESSION['CUSID'])){  
+
+                              echo ' <a href="'.web_root. 'customer/controller.php?action=addwish&proid='.$result->PROID.'" title="Add to wishlist"><i class="fa fa-plus-square"></i>Add to wishlist</a></a>
+                            ';
+
+                             }else{
+                               echo   '<a href="#" title="Add to wishlist" class="proid"  data-target="#smyModal" data-toggle="modal" data-id="'.  $result->PROID.'"><i class="fa fa-plus-square"></i>Add to wishlist</a></a>
+                            ';
+                            }  
+                            ?>
+
+                    </li> 
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </form>
+       <?php  } ?>
+            
+          </div><!--features_items--> 
+          
+          <div class="recommended_items"><!--recommended_items-->
+            <h2 class="title text-center">recommended items</h2>
+            
+            <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="item active"> 
+                         <?php 
+                    $query = "SELECT * FROM `tblpromopro` pr , `tblproduct` p , `tblcategory` c
+                    WHERE pr.`PROID`=p.`PROID` AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 limit 3 ";
+                    $mydb->setQuery($query);
+                    $cur = $mydb->loadResultList();
+                   
+                    foreach ($cur as $result) { 
+                  ?>
+                      <form   method="POST" action="cart/controller.php?action=add">
+            <input type="hidden" name="PROPRICE" value="<?php  echo $result->PROPRICE; ?>">
+            <input type="hidden" id="PROQTY" name="PROQTY" value="<?php  echo $result->PROQTY; ?>">
+
+            <input type="hidden" name="PROID" value="<?php  echo $result->PROID; ?>">
+                  <div class="col-sm-4">
+                    <div class="product-image-wrapper">
+                      <div class="single-products">
+                        <div class="productinfo text-center">
+                          <img src="<?php  echo web_root.'admin/products/'. $result->IMAGES; ?>" alt="" />
+                          <h2>Rs. <?php  echo $result->PRODISPRICE; ?></h2>
+                          <p><?php  echo    $result->PRODESC; ?></p>
+                           <button type="submit" name="btnorder" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                  <?php } ?>
+                </div>
+                <div class="item">  
+                  <?php 
+                    $query = "SELECT * FROM `tblpromopro` pr , `tblproduct` p , `tblcategory` c
+                    WHERE pr.`PROID`=p.`PROID` AND  p.`CATEGID` = c.`CATEGID`  AND PROQTY>0 limit 3,6";
+                    $mydb->setQuery($query);
+                    $cur = $mydb->loadResultList();
+                   
+                    foreach ($cur as $result) { 
+                  ?>
+                      <form   method="POST" action="cart/controller.php?action=add">
+            <input type="hidden" name="PROPRICE" value="<?php  echo $result->PROPRICE; ?>">
+            <input type="hidden" id="PROQTY" name="PROQTY" value="<?php  echo $result->PROQTY; ?>">
+
+            <input type="hidden" name="PROID" value="<?php  echo $result->PROID; ?>">
+                  <div class="col-sm-4">
+                    <div class="product-image-wrapper">
+                      <div class="single-products">
+                        <div class="productinfo text-center">
+                          <img src="<?php  echo web_root.'admin/products/'. $result->IMAGES; ?>" alt="" />
+                          <h2>Rs. <?php  echo $result->PRODISPRICE; ?></h2>
+                          <p><?php  echo    $result->PRODESC; ?></p>
+                           <button type="submit" name="btnorder" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                  <?php } ?>
+                </div>
+              </div>
+               <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
+                <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
+                <i class="fa fa-angle-right"></i>
+                </a>      
+            </div>
+          </div><!--/recommended_items-->
+
+          
+          
+        </div>
+      </div>
+    </div>
+    <hr>
+  </section>
+  <h1 style="text-align:center; color:rgb(243,145,39);">Customer Reviews</h1>
+  <section id="slider" style="margin-left:-100px;"><!--slider-->
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div id="slider-carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators" style="margin-left:-300px;">
+              <li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#slider-carousel" data-slide-to="1"></li>
+              <li data-target="#slider-carousel" data-slide-to="2"></li>
+            </ol>
+            
+            <div class="carousel-inner">
+              <div class="item active">
+                <div class="col-sm-12">
+                  <h1 style="font-size:24px; text-align:center;">Kasun Rajapakse</h1>
+                  <h2 style="text-align:center;">" Excellent Website for Online Grocery... "</h2><br><br>
+                  
+                 
+                </div>
+              </div>
+              <div class="item">
+                <div class="col-sm-12">
+                <h1 style="font-size:24px; text-align:center;">Udesh Perera</h1>
+                  <h2 style="text-align:center;">" Well Oragnized and User Friendly"</h2><br><br>
+
+                  
+                 
+                </div>
+
+              </div>
+              
+              <div class="item">
+                <div class="col-sm-12">
+                <h1 style="font-size:24px; text-align:center;">Lahiru Fernando</h1>
+                  <h2 style="text-align:center;">" Super Responsive... "</h2><br><br>
+
+                 
+                </div>
+
+              </div>
+              
+            </div>
+            
+            
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </section><!--/slider-->
